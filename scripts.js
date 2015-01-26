30d536b9fb5313e5850b61c101a60ea17221d0df
aalexeev239 <alexeev.andrey.a@gmail.com>
2015-01-26 03:23:00 +0300

    scripts added

--- scripts.js
+++ scripts.js
@@ -1,65 +1,116 @@
-// var myFeature = {
-//     init: function( settings ) {
-//         myFeature.config = {
-//             items: $( "#myFeature li" ),
-//             container: $( "<div class='container'></div>" ),
-//             urlBase: "/foo.php?item="
-//         };
+var haQuest = (function() {
+  var me = {};
+  var $fields = $('.quest__challenge');
+  var fieldsCount = $fields.length;
+  var result = {};
+  var prevBtn = $('#prevBtn');
+  var nextBtn = $('#nextBtn');
+  var resetBtn = $('#resetBtn');
+  var $panel = $('#panel');
 
-//         // Allow overriding the default config
-//         $.extend( myFeature.config, settings );
+  me.init = function () {
+    if (fieldsCount) {
+      // console.log($fields.find('select'));
+      resetForm();
 
-//         myFeature.setup();
-//     },
+      $fields.find('select').on('change', onSelectChanged);
 
-//     setup: function() {
-//         myFeature.config.items
-//             .each( myFeature.createContainer )
-//             .click( myFeature.showItem );
-//     },
+      prevBtn.on('click', -1, onNavBtnClicked);
+      nextBtn.on('click', 1, onNavBtnClicked);
+      resetBtn.on('click', 1, resetForm);
+    }
+  };
 
-//     createContainer: function() {
-//         var item = $( this );
-//         var container = myFeature.config.container
-//             .clone()
-//             .appendTo( item );
-//         item.data( "container", container );
-//     },
+  function resetForm() {
+    prevBtn.removeClass('hidden');
+    nextBtn.removeClass('hidden');
+    resetBtn.addClass('hidden');
+    $panel.find('.results').remove();
 
-//     buildUrl: function() {
-//         return myFeature.config.urlBase + myFeature.currentItem.attr( "id" );
-//     },
+    $fields.addClass('hidden').eq(0).removeClass('hidden');
+    prevBtn.addClass('disabled');
 
-//     showItem: function() {
-//         myFeature.currentItem = $( this );
-//         myFeature.getContent( myFeature.showContent );
-//     },
-
-//     getContent: function( callback ) {
-//         var url = myFeature.buildUrl();
-//         myFeature.currentItem.data( "container" ).load( url, callback );
-//     },
-
-//     showContent: function() {
-//         myFeature.currentItem.data( "container" ).show();
-//         myFeature.hideContent();
-//     },
-
-//     hideContent: function() {
-//         myFeature.currentItem.siblings().each(function() {
-//             $( this ).data( "container" ).hide();
-//         });
-//     }
-// };
-
-// $(document).ready( myFeature.init );
+    $fields.find('select').each(function(){
+      var name = this.name;
+      result[name] = 0;
+    });
+  }
 
 
-var haQuest = {
-  init: function() {
+
+  function onSelectChanged(e) {
+    var name = this.name;
+    var value = this.value;
+    var $self = $(this);
+    var field = $(this).closest('.quest__challenge')[0];
+    var index = $fields.indexOf(field);
+
+    if (field && index >=0 && name in result) {
+
+      result[name] = value;
+
+      goToSelect(field, index+1);
+    }
+  }
+
+
+
+  function goToSelect(curentField, index) {
+    var newField = $fields.get(index);
+
+    index === 0 && prevBtn.addClass('disabled');
+    prevBtn.hasClass('disabled') && index > 0 && prevBtn.removeClass('disabled');
+
+    // index === (fieldsCount - 1) && nextBtn.addClass('disabled');
+    // nextBtn.hasClass('disabled') && (index < (fieldsCount - 1)) && nextBtn.removeClass('disabled');
+
+    if (index < fieldsCount) {
+      curentField.classList.add('hidden');
+      newField.classList.remove('hidden');
+      return newField;
+    }
+
+    publishResult(result);
+  }
+
+
+
+  function onNavBtnClicked(e) {
+    e.preventDefault();
+    var currentField = $fields.not('.hidden')[0];
+    var index = $fields.indexOf(currentField);
+    if(! isNaN(e.data)) {
+      goToSelect(currentField, index + e.data);
+    }
 
   }
-}
 
-$(document).ready( haQuest.init );
 
+
+  function publishResult(result) {
+    console.log(result);
+    $fields.addClass('hidden');
+    prevBtn.addClass('hidden');
+    nextBtn.addClass('hidden');
+    resetBtn.removeClass('hidden');
+    var resultString = '<table class="results">';
+
+    for (var property in result) {
+      if (result.hasOwnProperty(property)) {
+        var value = result[property] || "нет ответа";
+        console.log(value, typeof value);
+        resultString = resultString + '<tr><td>'+property+'</td><td>'+ value +'</td></tr>';
+      }
+    }
+
+    resultString += '</table>';
+    // console.log(resultString);
+    $panel.append(resultString);
+  }
+
+  return me;
+}());
+
+Zepto(function($){
+  haQuest.init()
+})
